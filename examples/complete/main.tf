820303fd22ac234d6cbf6dbb1f4995c3fc0db0f1
tmknom <tmk.nom@gmail.com>
2018-10-28 20:08:17 +0900

    Implement complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,1 +1,14 @@
+module "iam_account" {
+  source        = "../../"
+  account_alias = "complete"
 
+  allow_users_to_change_password = true
+  minimum_password_length        = 32
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_symbols                = true
+  require_uppercase_characters   = true
+  max_password_age               = 0
+  hard_expiry                    = false
+  password_reuse_prevention      = 0
+}
