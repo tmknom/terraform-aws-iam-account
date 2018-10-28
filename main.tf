ad388bc1ca1d72f5d75a18085b922e45aa2a2cdf
tmknom <tmk.nom@gmail.com>
2018-10-28 11:22:46 +0900

    Implement IAM Account Password Policy

--- main.tf
+++ main.tf
@@ -13,3 +13,32 @@ resource "aws_iam_account_alias" "default" {
   # https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_iam-limits.html
   account_alias = "${var.account_alias}"
 }
+
+# There is only a single policy allowed per AWS account.
+# An existing policy will be lost when using this resource as an effect of this limitation.
+#
+# https://www.terraform.io/docs/providers/aws/r/iam_account_password_policy.html
+# https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_passwords_account-policy.html
+resource "aws_iam_account_password_policy" "default" {
+  # You can permit all IAM users in your account to use the IAM console to change their own passwords
+  allow_users_to_change_password = "${var.allow_users_to_change_password}"
+
+  # You can specify the minimum number of characters allowed in an IAM user password.
+  # You can type any number from 6 to 128.
+  minimum_password_length = "${var.minimum_password_length}"
+
+  # You can require that IAM user passwords contain at least one lowercase character
+  # from the ISO basic Latin alphabet (a to z).
+  require_lowercase_characters = "${var.require_lowercase_characters}"
+
+  # You can require that IAM user passwords contain at least one numeric character (0 to 9).
+  require_numbers = "${var.require_numbers}"
+
+  # You can require that IAM user passwords contain at least one of the following nonalphanumeric characters:
+  # ! @ # $ % ^ & * ( ) _ + - = [ ] { } | '
+  require_symbols = "${var.require_symbols}"
+
+  # You can require that IAM user passwords contain at least one uppercase character
+  # from the ISO basic Latin alphabet (A to Z).
+  require_uppercase_characters = "${var.require_uppercase_characters}"
+}
--- variables.tf
+++ variables.tf
@@ -6,3 +6,39 @@ variable "account_alias" {
   type        = "string"
   description = "The account alias."
 }
+
+variable "allow_users_to_change_password" {
+  default     = true
+  type        = "string"
+  description = "Whether to allow users to change their own password."
+}
+
+variable "minimum_password_length" {
+  default     = 6
+  type        = "string"
+  description = "Minimum length to require for user passwords."
+}
+
+variable "require_lowercase_characters" {
+  default     = true
+  type        = "string"
+  description = "Whether to require lowercase characters for user passwords."
+}
+
+variable "require_numbers" {
+  default     = true
+  type        = "string"
+  description = "Whether to require numbers for user passwords."
+}
+
+variable "require_symbols" {
+  default     = true
+  type        = "string"
+  description = "Whether to require symbols for user passwords."
+}
+
+variable "require_uppercase_characters" {
+  default     = true
+  type        = "string"
+  description = "Whether to require uppercase characters for user passwords."
+}
